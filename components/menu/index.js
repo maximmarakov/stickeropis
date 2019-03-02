80b20106e220ba6f61bed55f59b96f12e1e91328
maxim marakov <maximmarakov@yandex.ru>
2019-03-02 16:53:26 +0500

    fix lint Signed-off-by: maxim marakov <maximmarakov@yandex.ru>

--- components/menu/index.js
+++ components/menu/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
 import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 
 import { classname } from 'helpers/classname';
 
@@ -12,24 +10,27 @@ const b = classname('menu');
 
 class Menu extends Component {
     render() {
-        const items = ['trello', 'github', 'csv','jira','form'];
+        const items = ['trello', 'github', 'csv', 'jira', 'form'];
 
         return (
-          <div className={b()}>
-            <ul className={b('wrap')}>
-                {items.map(item => {
-                    return (
-                        <li key={item} className={b(`menu-item`)}>
-                            <Link href={`/${item}-config`} passHref>
-                              <Button className={b('imageBtn')}>
-                                <img className={b(`image`)} src={`static/images/${item}.png`}/>
-                              </Button>
-                            </Link>
-                        </li>
-                    );
-                })}
-            </ul>
-          </div>
+            <div className={b()}>
+                <ul className={b('wrap')}>
+                    {items.map(item => {
+                        return (
+                            <li key={item} className={b(`menu-item`)}>
+                                <Link href={`/${item}-config`} passHref>
+                                    <Button className={b('imageBtn')}>
+                                        <img
+                                            className={b(`image`)}
+                                            src={`static/images/${item}.png`}
+                                            />
+                                    </Button>
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
         );
     }
 }
